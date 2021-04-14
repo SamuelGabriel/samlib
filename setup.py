95e43adbe3331645259106e3e6389c1a834ff42f
Samuel Mueller <muellesa@tf.uni-freiburg.de>
2021-04-14 10:02:25 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='samlib',
+      version='0.0',
+      description='Personal Library of Samuel Müller',
+      url='',
+      author='Samuel Müller',
+      author_email='muellesa@tf.uni-freiburg.de',
+      license='MIT',
+      packages=['samlib'],
+      zip_safe=False)
